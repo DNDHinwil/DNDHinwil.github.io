f8d7795aa5bdda047c9400f6fd7ef7558558a0c5
Elias-Styner <elias.styner@gmail.com>
2026-09-16 00:46:46 +0200

    working on timer

---
+++ DNDHinwil.Website/Models/Player.cs
@@ -0,0 +1,6 @@
+﻿namespace DNDHinwil.Website.Models;
+
+public class Player
+{
+    public string Name { get; set; }
+}
--- DNDHinwil.Website/Services/ContentService.cs
+++ DNDHinwil.Website/Services/ContentService.cs
@@ -1,15 +1,25 @@
-﻿using System.Net.Http.Json;
+﻿using Microsoft.JSInterop;
+using System.Text.Json;
 
 namespace DNDHinwil.Website;
 
 public interface IContentService
 {
-
+    public ValueTask StoreData<TData>(string key, TData data);
+    public ValueTask<TData?> LoadData<TData>(string key);
 }
 
-public class ContentService(HttpClient client) : IContentService
+public class ContentService(HttpClient client, IJSRuntime js) : IContentService
 {
     private readonly HttpClient _client = client;
+    private readonly IJSRuntime _js = js;
 
-
+    public async ValueTask StoreData<TData>(string key, TData data)
+        => await _js.InvokeVoidAsync("localStorage.setItem",
+        [
+            key,
+            JsonSerializer.Serialize(data)
+        ]);
+    public async ValueTask<TData?> LoadData<TData>(string key)
+        => await _js.InvokeAsync<TData?>("localStorage.getItem", key);
 }
