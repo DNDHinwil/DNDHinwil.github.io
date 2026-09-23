0157e66ee34990443b44565bc4c44f920c71b5a2
Elias-Styner <elias.styner@gmail.com>
2026-09-24 00:55:00 +0200

    creating a line graph for turn time tracking

---
+++ DNDHinwil.Website/Models/Generic/LineGraphPoint.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+namespace DNDHinwil.Website.Models;
+
+public class LineGraphPoint<TValue>
+    where TValue: INumber<TValue>
+{
+    public string? Label { get; set; }
+    public required TValue Value { get; set; }
+}
--- DNDHinwil.Website/Program.cs
+++ DNDHinwil.Website/Program.cs
@@ -1,5 +1,4 @@
 using DNDHinwil.Website;
-using MudBlazor.Services;
 using IndexedDB.Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -12,6 +11,5 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddScoped<IDataService, DataService>();
 
 builder.Services.AddScoped<IIndexedDbFactory, IndexedDbFactory>();
-builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
